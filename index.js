5e29a46711ed37cb70caf2f0d59ad3f9b5f7b87c
manny <mannyis@typingona.computer>
2021-11-03 20:14:24 -0500

    working through scanning

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import * as readline from 'node:readline/promises'
 import * as path from 'node:path'
 import { stdin as input, stdout as output } from 'process'
 
-const hadError = false
+//may need to create a Lox class
+//will revisit later
+let hadError = false
 
 function main () {
   const args = process.argv.slice(2)
@@ -24,7 +26,9 @@ function main () {
 
 function runFile (filePath) {
   const normalizedFilePath = path.normalize(filePath)
-  runFile(normalizedFilePath)
+  run(normalizedFilePath)
+
+  if (hadError) process.exit(9)
 }
 
 main()
@@ -47,6 +51,48 @@ function runPrompt () {
   rl.prompt()
   rl.on('line', (line) => {
     rl.prompt()
-  // run(line);
+    run(line)
+    hadError = false
   })
+}
+
+/* private static void run(String source) {
+  Scanner scanner = new Scanner(source);
+  List<Token> tokens = scanner.scanTokens();
+
+  // For now, just print the tokens.
+  for (Token token : tokens) {
+    System.out.println(token);
+  }
+} */
+
+function run (source) {
+  const scanner = Scanner(source)
+  const tokens = scanner.scanTokens()
+
+  for (const token in tokens) {
+    console.log({ token })
+  }
+}
+
+/*
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where,
+                             String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+ */
+
+function error (lineNumber, message) {
+  report(lineNumber, '', message)
+}
+
+function report (lineNumber, location, message) {
+  console.error(`[line ${lineNumber}] Error ${location}: ${message}`)
+  hadError = true
 }
